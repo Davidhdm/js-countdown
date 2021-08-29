f780d4d4073ee512e46e7261d40e390232df3dd6
Davidhdm <david.hdm9@gmail.com>
2021-08-29 21:47:36 +0200

    added inputs for custom date/time and color variables

--- index.js
+++ index.js
@@ -1,19 +1,33 @@
+import fullInputValue from './inputs.js'
+console.log(fullInputValue)
 
-let countdownTime = new Date('Sep 8 2021 23:59:59').getTime()
+let countdownTime = null
+
+if (fullInputValue.length > 6) {
+  countdownTime = new Date(fullInputValue)
+} else {
+  countdownTime = new Date('09/08/2021 23:59:59')
+}
+
+console.log(countdownTime)
 
 let countdown = setInterval(() => {
-  let currentTime = new Date().getTime()
+  let currentTime = new Date()
   let timeLeft = countdownTime - currentTime
+
+  if (timeLeft < 0) {
+    timeLeft = 0
+  }
 
   let days = Math.floor(timeLeft / (1000 * 60 * 60 * 24))
   let hours = Math.floor((timeLeft % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60))
   let minutes = Math.floor((timeLeft % (1000 * 60 * 60)) / (1000 * 60))
   let seconds = Math.floor((timeLeft % (1000 * 60)) / 1000)
 
-  document.querySelector('.days').innerHTML = days
-  document.querySelector('.hours').innerHTML = hours
-  document.querySelector('.minutes').innerHTML = minutes
-  document.querySelector('.seconds').innerHTML = seconds
+  document.querySelector('.days').innerHTML = days || 0
+  document.querySelector('.hours').innerHTML = hours || 0
+  document.querySelector('.minutes').innerHTML = minutes || 0 
+  document.querySelector('.seconds').innerHTML = seconds || 0
 
   if (timeLeft <= 0) {
     clearInterval(countdown)
---
+++ inputs.js
@@ -0,0 +1,80 @@
+const dateInputBox = document.querySelector('.date-input-box')
+const dateLabel = document.querySelector('.date-label')
+const dateInput = document.querySelector('#date-input')
+
+const timeInputBox = document.querySelector('.time-input-box')
+const timeLabel = document.querySelector('.time-label')
+const timeInput = document.querySelector('#time-input')
+
+let dateInputFocus = false
+let timeInputFocus = false
+
+function hideLabel(label) {
+  label.classList.add('hidden')
+}
+
+function showLabel(label) {
+  label.classList.remove('hidden')
+}
+
+function setInputFocus(inputFocus, value) {
+  if (inputFocus === 'dateInputFocus') {
+    dateInputFocus = value
+  }
+
+  if (inputFocus === 'timeInputFocus') {
+    timeInputFocus = value
+  }
+}
+
+dateInput.addEventListener('focus', () => {
+  setInputFocus('dateInputFocus', true)
+})
+
+dateInput.addEventListener('blur', () => {
+  setInputFocus('dateInputFocus', false)
+  if (!dateInput.value || dateInput.value === '') {
+    showLabel(dateLabel)
+  }
+})
+
+dateInputBox.addEventListener('mouseover', () => {
+  hideLabel(dateLabel)
+})
+
+dateInputBox.addEventListener('mouseleave', () => {
+  if (!dateInputFocus && !dateInput.value) {
+    showLabel(dateLabel)
+  }
+})
+
+timeInput.addEventListener('focus', () => {
+  setInputFocus('timeInputFocus', true)
+})
+
+timeInput.addEventListener('blur', () => {
+  setInputFocus('timeInputFocus', false)
+  if (!timeInput.value || timeInput.value === '') {
+    showLabel(timeLabel)
+  }
+})
+
+timeInputBox.addEventListener('mouseover', () => {
+  hideLabel(timeLabel)
+})
+
+timeInputBox.addEventListener('mouseleave', () => {
+  if (!timeInputFocus && !timeInput.value) {
+    showLabel(timeLabel)
+  }
+})
+
+let fullInputValue = null
+
+if (!timeInput.value) {
+  fullInputValue = dateInput.value
+} else {
+  fullInputValue = dateInput.value + 'T' + timeInput.value
+}
+
+export default fullInputValue
